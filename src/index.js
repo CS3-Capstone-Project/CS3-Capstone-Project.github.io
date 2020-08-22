def1a41bf06da4ac1807f602c32f7f015ad062e7
Live Tembiso <tmbliv001@cs.uct.ac.za>
2020-08-22 11:35:51 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -2,10 +2,4 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from "./views/App.js";
 
-class App extends Component {
-  render() {
-    return <h1>Hello World!</h1>;
-  }
-}
-
 ReactDOM.render(<App/>, document.getElementById('root'));
