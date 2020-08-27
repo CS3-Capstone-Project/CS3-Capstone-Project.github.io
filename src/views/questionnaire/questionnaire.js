05bc833002eadcaf37e87c37ef4848727f213eb1
mdnyan002 <mdnyan002@myuct.ac.za>
2020-08-27 12:12:11 +0200

    Added Go back button

--- src/views/questionnaire/questionnaire.js
+++ src/views/questionnaire/questionnaire.js
@@ -6,6 +6,7 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
 import Dropbox from "./Components/dropbox.jsx";
 import LPButton from "./Components/LP_button.jsx";
+import HomeButton from "./Components/homeButton.jsx"
 
 export default class Questionnaire extends Component{
 	render(){
@@ -15,6 +16,8 @@ export default class Questionnaire extends Component{
                 <Dropbox/>
                 <p>There is a learning path specially made for you based on the level you selected.</p>
                 <LPButton/>
+                <li></li>
+                <HomeButton/>
             </ul>  
 		);
 	}
