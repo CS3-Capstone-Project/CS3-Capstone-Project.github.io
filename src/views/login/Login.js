8ca4d2533e363d99fab193f15e529f5c4de70e5e
Live Tembiso <tmbliv001@cs.uct.ac.za>
2020-08-28 23:47:00 +0200

    Added the logo

---
+++ src/views/login/Login.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+//React Bootstrap API
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
+
+
+export default class Login extends Component{
+	render(){
+		return(
+			<Container className="center-v">
+				<Row>
+					<Col>
+						Something
+					</Col>
+				</Row>
+			</Container>
+		);
+	}
+}
