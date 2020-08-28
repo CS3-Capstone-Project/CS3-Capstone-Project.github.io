86e79f9492c49182eb066af7f821cff5f94f3834
mdnyan002 <mdnyan002@myuct.ac.za>
2020-08-28 23:13:45 +0200

    added Beginner, Intermediate and Advanced pages

--- src/views/App.js
+++ src/views/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Questionnaire from "./questionnaire/questionnaire.js";
+import Beginner from "./questionnaire/Components/Beginner.js";
+import Intermediate from "./questionnaire/Components/Intermediate.js";
+import Advanced from "./questionnaire/Components/Advanced.js";
 import "./App.scss";
 
 //Views
-import Questionnaire from "./questionnaire/questionnaire.js";
-
 class App extends Component {
+
   render() {
     return (
-    	<Questionnaire/>
+    	<Router>
+                <div>
+                	<Switch>
+                		<Route exact path="/">
+	                            <Questionnaire/>
+	                    </Route>
+	                    <Route path="/Beginner">
+	                            <Beginner/>
+	                    </Route>
+	                    <Route path="/Intermediate">
+	                            <Intermediate/>
+	                    </Route>
+	                    <Route path="/Advanced">
+	                            <Advanced/>
+	                    </Route>
+                    </Switch>
+                </div>
+        </Router>
     );
   }
 }
--- src/views/questionnaire/questionnaire.js
+++ src/views/questionnaire/questionnaire.js
@@ -1,24 +1,72 @@
 import React, { Component } from "react";
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 //React Bootstrap API
-
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
-import Dropbox from "./Components/dropbox.jsx";
-import LPButton from "./Components/LP_button.jsx";
-import HomeButton from "./Components/homeButton.jsx"
+import {Container } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import './Components/LP_button.scss';
+import Beginner from "./Components/Beginner.js";
+
 
 export default class Questionnaire extends Component{
-	render(){
-		return(
+    style ={
+        fontSize: 17,
+        fontWeight: 'bold'
+    }
+    render() { 
+        return (
+            <div>
+                <Form>
+                    <Form.Label as="legend" column sm={2}>
+                        <p style={this.style}>Select level</p>
+                    </Form.Label>
+                    {['radio'].map((type) => (
+                        <div key={`default-${type}`} className="mb-3">
+                            <Form.Check
+                                type="radio"
+                                label="Beginner"
+                                name="formHorizontalRadios"
+                                id="formHorizontalRadios1"
+                            />
+                            <Form.Check
+                                type="radio"
+                                label="Intermediate"
+                                name="formHorizontalRadios"
+                                id="formHorizontalRadios2"
+                            />
+                            <Form.Check
+                                type="radio"
+                                label="Advanced"
+                                name="formHorizontalRadios"
+                                id="formHorizontalRadios2"
+                            />
+                        </div>
+                    ))}
+                </Form>
+                <div className="button-nav">
+                <Link to="/Beginner">
+                    <Button className="btn btn-default btn-sm">
+                        <span className="glyphicon glyphicon-chevron-down"></span>Take a learning path</Button>
+                </Link> 
+                </div>
+            </div>
+        );
+		
+	}
+}
 
-			<ul>
+
+
+
+/*return(
+
+            <Container>
                 <Dropbox/>
                 <p>There is a learning path specially made for you based on the level you selected.</p>
                 <LPButton/>
                 <li></li>
                 <HomeButton/>
-            </ul>  
-		);
-	}
-}
+            </Container>  
+        );*/
