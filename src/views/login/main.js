4dc49e7192960bc0a6de69c16ba5304f17efeb23
Xola Manciya <mncxol005@myuct.ac.za>
2020-08-29 17:09:32 +0200

    just deleted files and modified main.js

--- src/views/login/main.js
+++ src/views/login/main.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
-import './App.css';
+import './main.css';
 import { NavLink } from 'react-router-dom'
 import Loginscreen from './Loginscreen'
 class App extends Component {
