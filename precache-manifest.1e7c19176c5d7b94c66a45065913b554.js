bfaabb41e0453edb087744d59b13d13d0c864a3d
Live Tembiso <tmbliv001@cs.uct.ac.za>
2020-11-02 12:43:33 +0200

    Updates

---
+++ precache-manifest.1e7c19176c5d7b94c66a45065913b554.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1baaec6ea0d8aaf2f4c489e7788e4b35",
+    "url": "/pycat/index.html"
+  },
+  {
+    "revision": "b71f8fc52989d683c776",
+    "url": "/pycat/static/css/main.bf359007.chunk.css"
+  },
+  {
+    "revision": "ad3796f074862d6743fe",
+    "url": "/pycat/static/js/2.11febfa1.chunk.js"
+  },
+  {
+    "revision": "11aac78e400cca6a0bd15cf544030550",
+    "url": "/pycat/static/js/2.11febfa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b71f8fc52989d683c776",
+    "url": "/pycat/static/js/main.b9970006.chunk.js"
+  },
+  {
+    "revision": "362cf85035e8c4ddece3",
+    "url": "/pycat/static/js/runtime-main.39352e5c.js"
+  },
+  {
+    "revision": "87491ab560a48c3227adb5e9eb62b5d2",
+    "url": "/pycat/static/media/Login.87491ab5.svg"
+  }
+]);
